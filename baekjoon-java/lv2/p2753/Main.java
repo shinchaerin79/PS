9c37e4752b9469e74a1b2c2fc1235abaa236a596
shinchaerin79 <shinchaerin79@gmail.com>
2025-09-01 23:53:37 +0900

    :sparkles: Feat: [java-lv2] 1-3 추가

---
+++ baekjoon-java/lv2/p2753/Main.java
@@ -0,0 +1,18 @@
+package lv2.p2753;
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int year = sc.nextInt();
+
+    if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+      System.out.println("1");
+    } else {
+      System.out.println("0");
+    }
+    sc.close();
+  }
+}
