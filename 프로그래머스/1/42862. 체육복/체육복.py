817202e78d72e7c5aa5f6a52b6fe44bb01d00afc
shinchaerin79 <166346480+shinchaerin79@users.noreply.github.com>
2025-10-17 23:12:49 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -0,0 +1,7 @@
+def solution(n, lost, reserve):
+    l = set(lost) - set(reserve)
+    r = set(reserve) - set(lost)
+    for x in sorted(r):
+        if x-1 in l: l.remove(x-1)
+        elif x+1 in l: l.remove(x+1)
+    return n - len(l)
